8f93e571eddbfb29963d29a214779196fb1b368b
Ana Claudia <anacnogueira@gmail.com>
2023-09-15 13:10:24 -0300

    Comunicação entre camadas

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -4,7 +4,8 @@ import { CreateAccountController } from "./controllers/create-account.controller
 import { AuthenticateController } from "./controllers/authenticate.controller";
 import { CreateQuestionController } from "./controllers/create-question.controller";
 import { FetchRecentQuestionsController } from "./controllers/fetch-recent-question.controller";
-import { DatabaseModule } from "@faker-js/faker";
+import { DatabaseModule } from "../database/database.module";
+import { CreateQuestionUseCase } from "@/domain/forum/application/use-cases/create-question";
 
 @Module({
   imports: [DatabaseModule],
@@ -14,5 +15,6 @@ import { DatabaseModule } from "@faker-js/faker";
     CreateQuestionController,
     FetchRecentQuestionsController,
   ],
+  providers: [CreateQuestionUseCase],
 })
 export class HttpModule {}
