72833e6e30f88d89af749200ba275e57902c2e0c
Ana Claudia <anacnogueira@gmail.com>
2023-09-29 17:31:52 -0300

    Perguntas com anexo

--- test/repositories/in-memory-question-attachments-repository.ts
+++ test/repositories/in-memory-question-attachments-repository.ts
@@ -4,11 +4,21 @@ import { QuestionAttachment } from "@/domain/forum/enterprise/entities/question-
 export class InMemoryQuestionAttachmentsRepository
   implements QuestionAttachmentsRepository
 {
+  async createMany(attachments: QuestionAttachment[]): Promise<void> {
+    this.items.push(...attachments);
+  }
+
+  async deleteMany(attachments: QuestionAttachment[]): Promise<void> {
+    this.items = this.items.filter((item) => {
+      return !attachments.some((attachment) => attachment.equals(item));
+    });
+  }
+
   public items: QuestionAttachment[] = [];
 
   async findManyByQuestionId(questionId: string) {
     const questionAttachments = this.items.filter(
-      (item) => item.questionId.toString() === questionId,
+      (item) => item.questionId.toString() === questionId
     );
 
     return questionAttachments;
@@ -16,7 +26,7 @@ export class InMemoryQuestionAttachmentsRepository
 
   async deleteManyByQuestionId(questionId: string) {
     const questionAttachments = this.items.filter(
-      (item) => item.questionId.toString() !== questionId,
+      (item) => item.questionId.toString() !== questionId
     );
 
     this.items = questionAttachments;
